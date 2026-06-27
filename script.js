0950ea21d3de51a4fd9c6b96674277993218857d
Zeenat Eman <zeenateemankhang@gmail.com>
2026-06-27 04:45:54 -0700

    change

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function goToProducts() {
     document.getElementById("products").scrollIntoView({
         behavior: "smooth"
@@ -9,24 +8,8 @@ const subscribeBtn = document.querySelector(".newsletter button");
 
 subscribeBtn.onclick = function () {
     alert("Thank you for subscribing!");
-}; scroll
+};
 
 function subscribeUser() {
-    alert("Thank you for subscribing to Eman's Fashion Club!")
-}
-
-function goToProducts() {
-    document.getElementById("products").scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
-const subscribeBtn = document.querySelector(".newsletter button");
-
-subscribeBtn.onclick = function () {
-    alert("Thank you for subscribing!");
-}; scroll
-
-function subscribeUser() {
-    alert("Thank you for subscribing to Eman's Fashion Club!")
+    alert("Thank you for subscribing to Eman's Fashion Club!");
 }
