4d358c6cd52f372c8d8111d640327a6e7537d20e
Zeenat Eman <zeenateemankhang@gmail.com>
2026-06-27 03:23:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function goToProducts() {
+    document.getElementById("products").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const subscribeBtn = document.querySelector(".newsletter button");
+
+subscribeBtn.onclick = function () {
+    alert("Thank you for subscribing!");
+}; scroll
+
+function subscribeUser() {
+    alert("Thank you for subscribing to Eman's Fashion Club!");
+}
