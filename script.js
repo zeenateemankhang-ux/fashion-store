a286de0b7f7acceadd17198b8a2286fe87293602
Zeenat Eman <zeenateemankhang@gmail.com>
2026-06-27 06:16:33 -0700

    Change

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function goToProducts() {
 function subscribeUser() {
     alert("Thank you for subscribing to Eman's Fashion Club!");
 }
+
+let cart = 0;
+
+const buttons = document.querySelectorAll(".buy-btn");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        cart++;
+
+        document.getElementById("cartCount").innerText = cart;
+
+        alert("Product added to cart!");
+    });
+
+});
